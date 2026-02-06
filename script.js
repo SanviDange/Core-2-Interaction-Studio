93105360c5adf151c4cf61c3c589c1a13a5fae6c
SanviDange <dangs529@newschool.edu>
2026-02-06 01:59:16 +0000

    session 3.2

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// FADE ON HOMEPAGE
+
 setTimeout(function() {
   document.getElementById('splash-screen').classList.add('hidden');
   document.getElementById('main-content').classList.remove('hidden');
@@ -5,3 +7,14 @@ setTimeout(function() {
   document.body.style.overflow = 'auto';
 }, 15000);
 
+// IMAGE SWITCH
+// image id
+function changeImage(element) {
+  // Get just the filename from the current src
+  var currentSrc = element.src;
+  
+  // Replace -bw with -gold in the path
+  var newSrc = currentSrc.replace('-bw.png', '-gold.png');
+  
+  element.src = newSrc;
+}
