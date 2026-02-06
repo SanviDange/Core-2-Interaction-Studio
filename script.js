15d7486b742b87085c691d1b252d815d124eca26
SanviDange <dangs529@newschool.edu>
2026-02-06 01:27:11 +0000

    session 3

--- script.js
+++ script.js
@@ -3,4 +3,5 @@ setTimeout(function() {
   document.getElementById('main-content').classList.remove('hidden');
   document.getElementById('main-content').style.display = 'block'; // Explicitly set display
   document.body.style.overflow = 'auto';
-}, 3000);
+}, 15000);
+
